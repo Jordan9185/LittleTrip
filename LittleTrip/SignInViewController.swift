89a390e64e74d0db7de6f0b81cbb9f5c66863013
JordanLin <jordanlin@JordanLinde-MacBook-Air.local>
2017-07-25 17:32:20 +0800

    init first version

---
+++ LittleTrip/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignInViewController.swift
+//  LittleTrip
+//
+//  Created by JordanLin on 2017/7/25.
+//  Copyright © 2017年 JordanLin. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
