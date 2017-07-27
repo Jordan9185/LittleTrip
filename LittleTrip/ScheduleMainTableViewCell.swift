54011ea08999dcec409265f6e40a05e586ea89f8
JordanLin <jordanlin@JordanLinde-MacBook-Air.local>
2017-07-27 09:16:09 +0800

    Finished Main page UI.

---
+++ LittleTrip/ScheduleMainTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ScheduleMainTableViewCell.swift
+//  LittleTrip
+//
+//  Created by JordanLin on 2017/7/27.
+//  Copyright © 2017年 JordanLin. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleMainTableViewCell: UITableViewCell {
+
+    @IBOutlet var backgroundImageView: UIImageView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
