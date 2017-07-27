3b7719679449b45380e65ebfe9e947ff82966742
JordanLin <jordanlin@JordanLinde-MacBook-Air.local>
2017-07-27 16:21:07 +0800

    Implement dismiss function for CreateNewSchedule controller.

--- LittleTrip/CreateNewScheduleViewController.swift
+++ LittleTrip/CreateNewScheduleViewController.swift
@@ -12,8 +12,14 @@ class CreateNewScheduleViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        
+        self.dismiss(animated: true, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
