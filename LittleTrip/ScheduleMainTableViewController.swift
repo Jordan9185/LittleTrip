194dcaa2a34562053803b13d698ffaa4511abf13
JordanLin <jordanlin@JordanLinde-MacBook-Air.local>
2017-07-27 14:53:01 +0800

    Catch schedule data on firebase and show them.

--- LittleTrip/ScheduleMainTableViewController.swift
+++ LittleTrip/ScheduleMainTableViewController.swift
@@ -7,55 +7,102 @@
 //
 
 import UIKit
-import FirebaseStorage
+import FirebaseDatabase
+
+enum scheduleSection: Int {
+    
+    case mySchedule = 0
+    
+    case iAmJoining
+    
+}
+
+struct Schedule {
+    
+    let title: String
+    
+    let days: Int
+    
+    let createdDate: String
+    
+}
 
 class ScheduleMainTableViewController: UITableViewController {
 
+    var schedules: [Schedule] = []
+    
+    @IBOutlet var schedulesTableView: UITableView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
+        var scheduleRef: DatabaseReference!
         
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        scheduleRef = Database.database().reference().child("schedule")
+        
+        scheduleRef.observe(DataEventType.value, with: { (snapshot) in
+            
+            if let schedules = snapshot.value as? [[String:Any]] {
+            
+                for schedule in schedules {
+                    
+                    self.schedules.append(
+                        
+                        Schedule(
+                            title: schedule["title"] as! String,
+                            days: schedule["days"] as! Int,
+                            createdDate: schedule["createdDate"] as! String
+                        )
+                        
+                    )
+                    
+                }
+                
+                self.schedulesTableView.reloadData()
+                
+            }
+            
+        })
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+
         return 2
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 1
+        
+        if section == scheduleSection.mySchedule.rawValue {
+            
+            return schedules.count
+            
+        }
+        
+        return 0
+        
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "scheduleCell", for: indexPath) as! ScheduleMainTableViewCell
 
-        // Configure the cell...
-
+        cell.titleLabel.text = schedules[indexPath.row].title
+        
         return cell
+        
     }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         
         switch section {
             
-        case 0: return "My schedule"
+        case scheduleSection.mySchedule.rawValue : return "My schedule"
             
-        case 1: return "I am joining"
+        case scheduleSection.iAmJoining.rawValue : return "I am joining"
         
         default: return ""
             
@@ -63,49 +110,4 @@ class ScheduleMainTableViewController: UITableViewController {
         
     }
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
