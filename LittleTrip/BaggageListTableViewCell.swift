9cdf7ae6ce0a581c603014663fb3c7b44288d44d
JordanLin <jordanlin@JordanLinde-MacBook-Air.local>
2017-08-01 15:17:14 +0800

    Implement Baggage list UI.

---
+++ LittleTrip/BaggageListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BaggageListTableViewCell.swift
+//  LittleTrip
+//
+//  Created by JordanLin on 2017/8/1.
+//  Copyright © 2017年 JordanLin. All rights reserved.
+//
+
+import UIKit
+
+class BaggageListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
