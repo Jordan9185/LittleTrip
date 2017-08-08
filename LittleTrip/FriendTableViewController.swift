bec377a8c41d1262f68010538409569cd339a261
JordanLin <jordanlin@JordanLinde-MacBook-Air.local>
2017-08-08 15:06:38 +0800

    Add section for UID label.

--- LittleTrip/FriendTableViewController.swift
+++ LittleTrip/FriendTableViewController.swift
@@ -20,6 +20,14 @@ struct User {
     
 }
 
+enum sectionType {
+    
+    case myUID
+    
+    case friendList
+    
+}
+
 class FriendTableViewController: UITableViewController {
 
     var userListRef: DatabaseReference?
@@ -27,6 +35,8 @@ class FriendTableViewController: UITableViewController {
     let uid = (Auth.auth().currentUser?.uid)!
     
     var friends:[User] = []
+    
+    let sections: [sectionType] = [.myUID, .friendList]
     
     override func viewDidLoad() {
         
@@ -49,6 +59,8 @@ class FriendTableViewController: UITableViewController {
         self.userListRef = Database.database().reference().child("user")
         
         self.userListRef?.child(uid).child("friendList").observe(.value, with: { (snapshot) in
+            
+            self.friends = []
             
             if let friendIDs = snapshot.value as? [String] {
                 
@@ -87,12 +99,23 @@ class FriendTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
 
-        return 1
+        return sections.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch self.sections[section] {
+            
+        case .myUID:
+            
+            return 1
+            
+        case .friendList:
 
-        return self.friends.count
+            return self.friends.count
+            
+        }
+        
     }
 
     
@@ -100,36 +123,84 @@ class FriendTableViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath) as! FriendTableViewCell
 
-        cell.friendNameLabel.text = self.friends[indexPath.row].name
+        switch self.sections[indexPath.section] {
+            
+        case .myUID:
         
-        cell.userImageView.sd_setImage(with: URL(string: self.friends[indexPath.row].pictureURL))
+            cell.friendNameLabel.text = "My UID: \(uid)"
+            
+            cell.friendNameLabel.textAlignment = .center
+            
+            cell.userImageView.isHidden = true
+            
+        case .friendList:
+            
+            cell.friendNameLabel.text = self.friends[indexPath.row].name
+            
+            cell.userImageView.sd_setImage(with: URL(string: self.friends[indexPath.row].pictureURL))
+            
+        }
         
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        switch self.sections[section] {
+            
+        case .myUID:
+            
+            return nil
+            
+        case .friendList:
+            
+            return "Friend List (\(self.friends.count))"
+            
+        }
+        
+    }
+    
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
-        let viewWidth = self.view.frame.width
+        switch self.sections[section] {
+            
+        case .myUID:
         
-        let headerView = UIView(frame: CGRect(x: 0 , y: 0, width: self.view.frame.width, height: 84))
+            let headerView = UIView(frame: CGRect(x: 0 , y: 0, width: self.view.frame.width, height: 84))
         
-        let labelView = UILabel(frame: CGRect(x: 0 , y: 0, width: self.view.frame.width, height: 84))
+            let labelView = UILabel(frame: CGRect(x: 0 , y: 0, width: self.view.frame.width, height: 84))
         
-        labelView.text = "Friend List"
+            labelView.text = "Friend List"
         
-        labelView.textAlignment = .center
+            labelView.textAlignment = .center
         
-        labelView.backgroundColor = UIColor.yellow
+            labelView.backgroundColor = UIColor.yellow
         
-        headerView.addSubview(labelView)
+            headerView.addSubview(labelView)
         
-        return headerView
+            return headerView
+        
+        case .friendList:
+            
+            return nil
+            
+        }
         
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         
-        return 84
+        switch self.sections[section] {
+            
+        case .myUID:
+            
+            return 84
+            
+        case .friendList:
+            
+            return 40
+            
+        }
         
     }
     
