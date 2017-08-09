ae01aa6d04dc2835428a499767c5e814fa6c7b48
JordanLin <jordanlin@JordanLinde-MacBook-Air.local>
2017-08-09 16:06:39 +0800

    Implement Parner Board UI.

---
+++ LittleTrip/ParnerBoardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ParnerBoardTableViewCell.swift
+//  LittleTrip
+//
+//  Created by JordanLin on 2017/8/9.
+//  Copyright © 2017年 JordanLin. All rights reserved.
+//
+
+import UIKit
+
+class ParnerBoardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
