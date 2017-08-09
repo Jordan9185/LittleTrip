61e1a81b306fc7b8e7d5addfb6161154bc65654e
JordanLin <jordanlin@JordanLinde-MacBook-Air.local>
2017-08-09 17:24:06 +0800

    Add collection View for users pic.

---
+++ LittleTrip/ParnerCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ParnerCollectionViewCell.swift
+//  LittleTrip
+//
+//  Created by JordanLin on 2017/8/9.
+//  Copyright © 2017年 JordanLin. All rights reserved.
+//
+
+import UIKit
+
+class ParnerCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var parnerPicImageView: UIImageView!
+}
